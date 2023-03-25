3e98cc92d0d578529284765a6c135c98c22a347e
keymiro <44626685+keymiro@users.noreply.github.com>
2023-03-24 20:09:07 -0500

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -54,8 +54,12 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header bg-dark text-white text-center" style="display: block;">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Formulario de contacto</h1>
-                <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">
+                    Formulario de contacto
+                    <a type="button" href="" data-bs-dismiss="modal" aria-label="Close" class="ms-5 link-danger">
+                        <i class="fa-solid fa-circle-xmark text-danger"></i>
+                    </a>
+                </h1>
             </div>
             <div class="modal-body">
                 <form class="row g-3">
